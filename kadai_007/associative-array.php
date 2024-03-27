2ca98b147aa6ffce422f605303fc7478dfc7113d
1710shichu <157471262+1710shichu@users.noreply.github.com>
2024-03-28 00:02:52 +0900

    teishutu

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題７</title>
+</head>
+<body>
+  <?php
+    $vegitable_data = ['name' =>'onion', 'price' =>'200', 'weight' =>'160'];
+  
+    print_r($vegitable_data);
+  ?>
+</body>
+</html>
